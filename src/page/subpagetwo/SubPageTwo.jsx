99fa64f991a061adef2b7daba6cc74bedd97ba8a
tobias10kodehode <tobias10.kodehode@gmail.com>
2024-01-09 14:35:05 +0100

    *

--- src/page/subpagetwo/SubPageTwo.jsx
+++ src/page/subpagetwo/SubPageTwo.jsx
@@ -1,10 +1,12 @@
 import "./subpagetwo.css"
 export const SubPageTwo = () => {
   return (
+  <body>
     <h1>
       This is the second sub page. You need to do a fetch here from
       https://jsonplaceholder.typicode.com/ (choose one of the endpoints, but
       different from the one in SubPageOne)
     </h1>
+  </body>
   );
 };
